c3cfee4aaf2e0a548a47f5cd212520ea2c567434
kyle webb <kylewebb4@weber.edu>
2017-02-14 16:39:11 +0000

    Initial commit

---
+++ pions_hw5_task1.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  pions_hw5_task1.c
+ *       	Usage:  ./pions_hw5_task1.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 04:37:33 PM
+ *       Compiler:  gcc -Wall -Werr
+ *         Author:  Kyle Webb (), kylewebbear@weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(void)
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+

823e8c624ea8cc22596de6c73d09b2dc526b6b2c
kyle webb <kylewebb4@weber.edu>
2017-02-16 18:12:26 +0000

    Captured R1 & R2

--- pions_hw5_task1.c
+++ pions_hw5_task1.c
@@ -12,17 +12,25 @@
  * =====================================================================================
  */
 #include <stdio.h>		/* For Standard I/O */
+#include <math.h>		/* For Math library */
 
 /* Function Prototypes */
+void AnnulusMoment(float am);
 
 /* Main Program */
 int main(void)
 {
+	float r1, r2;
+
+	printf("Enter the value for the outer radius (r2): ");
+	scanf("%f", &r2);
+	printf("Enter the value for the inner radius (r1): ");
+	scanf("%f", &r1);
 
 	return 0;
 }
 
-
 /* Function Defenitions */
+
 
 
